f9990fde7a22259c5df57b1331699a080a6d6e92
zkemp1 <36314997+zkemp1@users.noreply.github.com>
2019-05-02 18:38:47 -0400

    Update SSIM.ino

--- SSIM/SSIM.ino
+++ SSIM/SSIM.ino
@@ -12,16 +12,37 @@ float peak; //peak set by TCU given function
 int phase; //1 or -1
 float t; //counter for time?
 
-  
+uint8_t bytes[3];
+
+#define SDA_PIN 4
+#define SCL_PIN 5
+
+const int16_t I2C_MASTER = 0x42;
+const int16_t I2C_SLAVE = 0x08;  
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);           // start serial for output (serial used for plot)
+  Wire.begin(SDA_PIN, SCL_PIN, I2C_SLAVE); // new syntax: join i2c bus (address required for slave)
+  Wire.onReceive(receiveEvent); // register event
 }
 
 void loop() {
-  //get code from TCU
-
-  //set peak, boolean array, phase shift 
+  void receiveEvent(size_t howMany) {
+  int i = 0; 
+  (void) howMany;
+  while (1 < Wire.available()) { // loop through all but the last
+    bytes[i]  = Wire.read(); // receive byte as a character
+    //Serial.print(bytes[i]);         // print the character
+    ++i;
+  }
   
+  //determine function type
+    
+  //determine phase shift
+    
+  //determine timing
+    
   //Simulate signal
   volt = phase*peak*sin(2*3.14159*FREQ*t)*sin((TR-TS)/(1.12*BW))/((TR-TS)/(1.12*BW));
 }
+
