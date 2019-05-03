6151ca6ba661806239a99c11305aad874aba613a
Griffin Bonner <34168079+GriffinBonner@users.noreply.github.com>
2019-05-03 15:49:29 -0400

    timing architecture

--- TCU/MLS_TCU/MLS_TCU.ino
+++ TCU/MLS_TCU/MLS_TCU.ino
@@ -4,7 +4,8 @@
  * Description: See TCU System Design Document (SDD) 
  */
 
-#include <stdbool.h>  
+#include <stdbool.h>                                                      
+#include <timer.h>                                                        // Timing library
 #include <SPI.h>                                                          // SPI interface library
 #include <Wire.h>                                                         // I2C interface library
 #include <Adafruit_SPIFlash.h>                                            // SPI flash library
@@ -33,7 +34,7 @@ Adafruit_W25Q16BV_FatFs fatfs(flash);
 /* ----------------- Signal Definitions -------------- */
 const int SLAVE_SELECT = 10;                                               // Active low slave select control signal
 const int TIME_SYNC_OUT = 3;                                               // Time synchronization output (1ms positive pulse corresponding to time-zero of transmission cycle)
-      
+Timer<1, micros> TX_SEQ;
 
 
 /* ----------------- TCU Functions -------------------- */
@@ -47,52 +48,7 @@ void serial_init(){
     }
 }
 
-// Initialize - Setup fatfs flash memory filesystem
-void fatfs_init(){
 
-  // Initialize SPI Flash
-  if(!flash.begin(FLASH_TYPE)){
-    Serial.println("Error, failed to initialize Flash Memory");
-    while(1);
-  }
-  Serial.print("Flash chip JEDEC ID: 0x"); Serial.println(flash.GetJEDECID(), HEX);
-
-  // Mount fatfs Flash filesystem
-  if(!fatfs.begin()){
-    Serial.println("Error mounting filesystem");
-    while(1);
-  }
-  Serial.println("Filesystem Mounted...");
-
-  // Verify-Create /site_personality directory
-  if(!fatfs.exists("/site_personality")){
-    Serial.println("site_personality directory not found, creating...");
-    if(!fatfs.mkdir("/site_personality")){
-      Serial.println("Error, failed to create site_personality directory");
-      while(1);
-    }
-    Serial.println("Created site_personality directory");
-  }
-  
-  File writeFile = fatfs.open("/site_personality/tcu_constants.txt", FILE_WRITE);
-  if(!writeFile){
-    Serial.println("Error, failed to open tcu_constants.txt for writing");
-      while(1);
-  }else{
-    Serial.println("Opened file site_personality/tcu_constants.txt for writing...");
-    // TODO: Write TCU data to tcu_constants flash memory file
-    writeFile.close();
-  }
-
-  // Open tcu_constants.txt file for reading
-  File readFile = fatfs.open("site_personality/tcu_constants.txt", FILE_READ);
-  if (!readFile) {
-    Serial.println("Error, failed to open test.txt for reading!");
-    while(1);
-  }else{
-    Serial.println("Opened file stite_personality/tcu_constants.txt for reading...");
-  }
-}
 
 
 /* ----------------- TCU Setup -------------------- */
@@ -109,21 +65,10 @@ void setup() {
 
 void loop(){
 
+  TX_SEQ.tick();                                                          // Full transmission sequence timer (615 ms period)
+
   digitalWrite(TIME_SYNC_OUT, HIGH);                                      // 1ms positive pulse
 
 }
 
-// TODO: Initialize timer overflow ISR for full transmission sequence (615 ms)
-void TRANS_ISR(){
 
-}
-
-// TODO: Initialize timer overflow ISR for AZ, BAZ, EL functions
-void FUNC_ISR(){
-  
-}
-
-// TODO: Write constants for transmission sequence ~ AZ, BAZ, EL functions to SPI Flash Memory
-void TCU_WriteConstants(){
-
-}
