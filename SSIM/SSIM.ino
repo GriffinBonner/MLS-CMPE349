423c3dfaa7b76441415f1030bcebf01b917900df
zkemp1 <36314997+zkemp1@users.noreply.github.com>
2019-05-03 19:45:38 -0400

    Update SSIM.ino

--- SSIM/SSIM.ino
+++ SSIM/SSIM.ino
@@ -4,14 +4,12 @@ I2c protocal utilizes with wire library
 Authors: Zak Kempler, Jd Bryon, Taylor Bear, Cole Schmidt
 
 Reference code ----------
-slave receiver: https://github.com/esp8266/Arduino/blob/master/libraries/Wire/examples/slave_receiver/slave_receiver.ino
-master sender: https://github.com/esp8266/Arduino/blob/master/libraries/Wire/examples/master_writer/master_writer.ino
+master sender / slave receiver: https://www.arduino.cc/en/Tutorial/MasterWriter
 plot://https://learn.adafruit.com/experimenters-guide-for-metro/circ08-using%20the%20arduino%20serial%20plotter
 */
-#include <Wire.h>         // I2C interface library
-#include <MsTimer2.h>     // Ms2 Timer Library
-#include <stdint.h>
 
+#include <Wire.h>         // I2C interface library
+#include <stdint.h>
 
 const float FREQ = 6.0; //frequency in khz
 const float BW = 10; //3db beamwidth in degrees
@@ -36,9 +34,9 @@ const int16_t I2C_SLAVE = 0x08;
 float Tzero[3] = {6800, 4800, 33500};
 
 void setup() {
-  Serial.begin(115200);           // start serial for output (serial used for plot)
-  Wire.begin(SDA_PIN, SCL_PIN, I2C_SLAVE); // new syntax: join i2c bus (address required for slave)
+  Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
 }
 
 void loop() {
@@ -50,9 +48,15 @@ void loop() {
     //Serial.print(bytes[i]);         // print the character
     ++i;
   }
-  
+    
+  //reset antennas
+  antennas[0] == 0;
+  antennas[1] == 0;
+  antennas[2] == 0;
+    
   //determine function type, then set antenna array
   function = byte[0]&0b11111110;
+    
   if(function == 0x32) //AZ function
   {
     antennas[0] == 1;
@@ -104,7 +108,14 @@ void loop() {
   }
       
   //Simulate signal
-  volt = phase*peak*sin(2*3.14159*FREQ*t)*sin((TR-TS)/(1.12*BW))/((TR-TS)/(1.12*BW));
+  if(antennas[0] == 1 || antennas[2] == 1 || antennas[2] == 1 )
+  {
+    volt = phase*peak*sin(2*3.14159*FREQ*t)*sin((TR-TS)/(1.12*BW))/((TR-TS)/(1.12*BW));
+  }
+  else 
+  {
+    volt = 0;
+  }
     
   Serial.println(volt); //plot signal
   Serial.print(" "); 
