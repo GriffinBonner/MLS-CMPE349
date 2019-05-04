71e523ea48ff84c957da8c50feb8713649540c32
zkemp1 <36314997+zkemp1@users.noreply.github.com>
2019-05-03 21:44:18 -0400

    Update SSIM.ino

--- SSIM/SSIM.ino
+++ SSIM/SSIM.ino
@@ -8,7 +8,11 @@ plot://https://learn.adafruit.com/experimenters-guide-for-metro/circ08-using%20t
 */
 
 #include <Wire.h>         // I2C interface library
+#include <timer.h>        // timer library
 #include <stdint.h>
+
+auto timer = timer_create_default(); 
+int time = 0;
 
 const float FREQ = 6.0; //frequency in khz
 const float BW = 10; //3db beamwidth in degrees
@@ -16,8 +20,7 @@ const float SCANVEL = 2.0; //degree per microsecond
 float TR = 10; //angle of plane's receiver in the coverage volume 
 float TS; //scanning beam angle (should be function of time)
 double volt; //output voltage
-bool antennas[3]; //0:AZ 1:BAZ 2:EL
-float peak = 10; //peak set by TCU given function
+bool antennas[3]; //0:AZ 1:BAZ 2:E
 int phase; //1 or -1
 float t; //counter for time?
 int timedelay;
@@ -36,6 +39,7 @@ void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
+  timer.every(1/FREQ, increment_time);
 }
 
 void receiveEvent(int howMany) {
@@ -46,8 +50,15 @@ void receiveEvent(int howMany) {
     ++i;
   }
 }
+
+bool increment_time(void *){
+  time++;
+  return true;
+}
   
 void loop() {
+  
+  timer.tick();
     
   //reset antennas
   antennas[0] == 0;
@@ -110,7 +121,7 @@ void loop() {
   //Simulate signal
   if(antennas[0] == 1 || antennas[2] == 1 || antennas[2] == 1 )
   {
-    volt = phase*peak*sin(2*3.14159*FREQ*t)*sin((TR-TS)/(1.12*BW))/((TR-TS)/(1.12*BW));
+    volt = phase*sin(time)*sin((TR-TS)/(1.12*BW))/((TR-TS)/(1.12*BW));
   }
   else 
   {
